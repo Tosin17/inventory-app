eae5715e4fd1a7e461f7b4206d9765afb3f1fde4
Tosin <kajopelayeola@gmail.com>
2019-06-03 20:14:18 -0400

    Add initial routes

--- inventory/src/app/app-routing.module.ts
+++ inventory/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DevicesComponent } from './devices/devices.component';
+import { AppListComponent } from './app-list/app-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DevicesComponent
+  },
+  {
+    path: 'apps-list',
+    component: AppListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
