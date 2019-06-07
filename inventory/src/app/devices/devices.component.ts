171456951dd0753c543bdcb7f89a36adcd0ec4e7
Tosin <kajopelayeola@gmail.com>
2019-06-06 21:48:26 -0400

    Custom observable

--- inventory/src/app/devices/devices.component.ts
+++ inventory/src/app/devices/devices.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DevicesService } from './devices.service';
 import { DeviceModel } from '../models/device.model';
+import { ImageService } from '../services/image.service';
 
 @Component({
   selector: 'app-devices',
@@ -9,12 +10,21 @@ import { DeviceModel } from '../models/device.model';
 })
 export class DevicesComponent implements OnInit {
   private selectedDevice: DeviceModel;
-  constructor(private devicesService: DevicesService) { }
+  constructor(private devicesService: DevicesService, private imageService: ImageService) { }
 
   ngOnInit() {
     this.devicesService.selectedDevice
     .subscribe((device: DeviceModel) => {
       this.selectedDevice = device;
+    })
+
+    const obs$ = this.imageService.getIntervals()
+    .subscribe(data => {
+      console.log(data);
+
+      if (data === 10) {
+        obs$.unsubscribe();
+      }
     })
   }
 
---
+++ inventory/src/app/services/image.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({providedIn: 'root'})
+export class ImageService {
+    private customObservable$ = Observable.create(obs => {
+        let count = 1;
+        setInterval(() => {
+            obs.next(count);
+            count++;
+        }, 1000)
+    });
+
+    getIntervals() {
+        return this.customObservable$;
+    }
+}
