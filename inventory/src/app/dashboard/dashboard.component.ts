e19201032afeac74d0199b615412a5261905bc85
Tosin <kajopelayeola@gmail.com>
2019-06-26 20:14:36 -0400

    Add graphs

--- inventory/src/app/dashboard/dashboard.component.ts
+++ inventory/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   private type: string = 'line';
@@ -11,15 +11,76 @@ export class DashboardComponent implements OnInit {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: "Initial dataset",
-        data: [65, 59, 80, 81, 56, 55, 40]
+        label: "",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: '#36a2eb',
+        pointBackgroundColor: "#000000"
       }
     ]
   };
   private options: object = {
     responsive: true,
-    maintainAspectRatio: false
+    maintainAspectRatio: false,
+    elements: {
+      line: {
+        tension: 0 // disables bezier curves
+      }
+    },
+    legend: {
+      display: false
+    },
   };
+
+  private lineOptions = {
+    ...this.options,
+    scales: {
+      xAxes: [{
+        display: false
+      }],
+      yAxes: [{
+        display: false
+      }]
+    }
+  };
+
+  private barOptions = {
+    ...this.options,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+        },
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false,
+        },
+      }]
+    }
+  };
+  private barData = {
+    labels: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'July',
+      'Aug',
+      'Sep',
+      'Nov',
+      'Dec'
+    ],
+    datasets: [
+      {
+        label: "",
+        data: [65, 59, 80, 81, 56, 55, 40, 35, 76, 23, 88, 47],
+        backgroundColor: '#36a2eb',
+      }
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
